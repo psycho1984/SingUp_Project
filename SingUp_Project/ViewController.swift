6560b3f57958d2446b84a003ede1b5def1226665
avi <aviledovsky@gmail.com>
2016-10-30 02:14:58 -0700

    Initial Commit

---
+++ SingUp_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingUp_Project
+//
+//  Created by aviledovsky on 30/10/2016.
+//  Copyright © 2016 aviledovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
